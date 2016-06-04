9ebb0044c6b33e9a903f386ce2295b114d8b3212
Walker Dorris <walker.dorris@gmail.com>
2016-06-04 10:44:12 -0700

    Code First Migrations enabled; Scaffolding migration 'InitialCreate'

---
+++ DAL/RazzleContext.cs
@@ -0,0 +1,16 @@
+﻿using Razzle.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Razzle.DAL
+{
+    public class RazzleContext: DbContext
+    {
+        public DbSet<Player> Players { get; set; }
+        public DbSet<Turn> Turns { get; set; }
+        public DbSet<Game> Games { get; set; }
+    }
+}
---
+++ DAL/RazzleRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Razzle.DAL
+{
+    public class RazzleRepository
+    {
+        public RazzleContext context { get; set; }
+
+        public RazzleRepository()
+        {
+            //Needing an instance of Context
+            context = new RazzleContext();
+        }
+    }
+}
