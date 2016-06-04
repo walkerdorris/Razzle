f9b31ead4ff6109002390b412eb2b00dae37ddfd
Walker Dorris <walker.dorris@gmail.com>
2016-06-04 12:19:22 -0700

    methods added getplayer/game/turn and count

--- DAL/RazzleRepository.cs
+++ DAL/RazzleRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Razzle.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -13,6 +14,36 @@ namespace Razzle.DAL
         {
             //Needing an instance of Context
             context = new RazzleContext();
+        }
+
+        public int GetPlayerCount()
+        {
+            return context.Players.Count();
+        }
+
+        public int GetTurnCount()
+        {
+            return context.Turns.Count();
+        }
+
+        public int GetGameCount()
+        {
+            return context.Turns.Count();
+        }
+
+        public List<Player> GetPlayers()
+        {
+            return context.Players.ToList<Player>();
+        }
+
+        public List<Turn> GetTurns()
+        {
+            return context.Turns.ToList<Turn>();
+        }
+
+        public List<Game> GetGames()
+        {
+            return context.Games.ToList<Game>();
         }
     }
 }
