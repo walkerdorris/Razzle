98a684ea53d9791e1257e282930831954f756d8f
Walker Dorris <walker.dorris@gmail.com>
2016-06-10 23:23:43 -0700

    constructed basic functioning for rolling the dice and setting up the gameboard

--- Razzle/Scripts/app.js
+++ Razzle/Scripts/app.js
@@ -28,7 +28,53 @@ app.controller("GameCtrl", function () {
     var die_fifteen = ["E","L","P","S","T","U"];
     var die_sixteen = ["G","I","L","R","U","W"];
     
-    //PICKING RANDOM STRING FROM ONE DIE TEST
-    var die_roll = die_one[Math.floor(Math.random() * die_one.length)];
+    //PICKING RANDOM STRING FROM DICE
+    var die_roll_one = die_one[Math.floor(Math.random() * die_one.length)];
+    var die_roll_two = die_two[Math.floor(Math.random() * die_two.length)];
+    var die_roll_three = die_three[Math.floor(Math.random() * die_three.length)];
+    var die_roll_four = die_four[Math.floor(Math.random() * die_four.length)];
+    var die_roll_five = die_five[Math.floor(Math.random() * die_five.length)];
+    var die_roll_six = die_six[Math.floor(Math.random() * die_six.length)];
+    var die_roll_seven = die_seven[Math.floor(Math.random() * die_seven.length)];
+    var die_roll_eight = die_eight[Math.floor(Math.random() * die_eight.length)];
+    var die_roll_nine = die_nine[Math.floor(Math.random() * die_nine.length)];
+    var die_roll_ten = die_ten[Math.floor(Math.random() * die_ten.length)];
+    var die_roll_eleven = die_eleven[Math.floor(Math.random() * die_eleven.length)];
+    var die_roll_twelve = die_twelve[Math.floor(Math.random() * die_twelve.length)];
+    var die_roll_thirteen = die_thirteen[Math.floor(Math.random() * die_thirteen.length)];
+    var die_roll_fourteen = die_fourteen[Math.floor(Math.random() * die_fourteen.length)];
+    var die_roll_fifteen = die_fifteen[Math.floor(Math.random() * die_fifteen.length)];
+    var die_roll_sixteen = die_sixteen[Math.floor(Math.random() * die_sixteen.length)];
+
+    //UNSHUFFLED GAMEBOARD
+    var unshuffled_gameboard = [die_roll_one, die_roll_two, die_roll_three, die_roll_four,
+        die_roll_five, die_roll_six, die_roll_seven, die_roll_eight, die_roll_nine, die_roll_ten,
+        die_roll_eleven, die_roll_twelve, die_roll_thirteen, die_roll_fourteen, die_roll_fifteen,
+        die_roll_sixteen]
+
+    //SHUFFLE FUNCTION
+    function shuffle(array) {
+        var currentIndex = array.length, temporaryValue, randomIndex;
+
+        // While there remain elements to shuffle...
+        while (0 !== currentIndex) {
+
+            // Pick a remaining element...
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex -= 1;
+
+            // And swap it with the current element.
+            temporaryValue = array[currentIndex];
+            array[currentIndex] = array[randomIndex];
+            array[randomIndex] = temporaryValue;
+        }
+
+        return array;
+    }
+
+    var shuffled_gameboard = shuffle(unshuffled_gameboard);
+    
+
+    self.GameBoard = shuffled_gameboard;
 
 });
