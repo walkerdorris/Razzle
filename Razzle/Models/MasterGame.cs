4ada3f7842b0cfa99bc9e326c03981f62a728624
Walker Dorris <walker.dorris@gmail.com>
2016-06-20 19:26:46 -0700

    starting to redo databasew/ new class

---
+++ Razzle/Models/MasterGame.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Razzle.Models
+{
+    public class MasterGame
+    {
+        public int MasterGameId { get; set; }
+        public string Players { get; set; }
+        public string[] BestWords { get; set; }
+        public int[] Points { get; set; }
+
+    }
+}
